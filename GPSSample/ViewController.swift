8b62ea3c1c45bbd75484844148685ad965f85541
David Hanley <david.hanley12@btinternet.com>
2016-08-23 21:18:09 +0100

    Initial Commit

---
+++ GPSSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GPSSample
+//
+//  Created by David Hanley on 23/08/2016.
+//  Copyright © 2016 David Hanley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
